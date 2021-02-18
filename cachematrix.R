b8efb400061c9b4c0f9251764ad39a82e9fd770c
Your Name <you@example.com>
2021-02-17 20:47:22 -0800

    commit assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## to initiate the inverse 
+    m <- NULL
+    
+    ## This is meant to set the matrix
+    set <- function(y){
+            x <<- y
+            m <<- NULL
+    }
+    
+    ## This is to get the matrix
+    get <- function()x
+    
+    ## To set the inverse of the matrix
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function()m
+    
+    ## Return a list of the methods
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
@@ -12,4 +31,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    m <- x$getInverse()
+    
+    ## If statment to return the inverse if its already set
+    if(!is.null(m)){
+      message("getting cached data")
+      return(m)
+    }
+    
+    ## To get the matix
+     data <- x$get()
+     
+     ## Calculate the inverse
+     m <- solve(data,...)
+     
+     ## Set the inverse to the object
+     x$setInverse(m)
+     
+     ## Return the matrix
+     m
+  }
